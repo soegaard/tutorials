85444456658d32fb4274c6d9f3d96d063e48cefd
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-06-17 21:01:11 +0200

    Added comment to top-interaction example

--- macro-tutorial/top-interaction.rkt
+++ macro-tutorial/top-interaction.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(provide m n #%top-interaction)
 
 (begin-for-syntax (define a 42))
 (define-syntax (m stx) (with-syntax ([v a]) #''v))
@@ -22,10 +23,28 @@
 (begin-for-syntax
   (define first-interaction? #t))
 
-(define-syntax (#%top-interaction stx)
+#;(define-syntax (#%top-interaction stx)
   (syntax-case stx ()
     [(_ . expr)
      (when first-interaction?
-       (set! a 43)
-       (set! first-interaction? #f))
+       (set! first-interaction? #f)
+       (set! a 43))
      #'expr]))
+
+; this works in the repl! 
+; ... but it is not an ideal solution. What happens if this module
+; isn't used in a repl, but from is required from another module?
+
+(define-syntax (n2 stx)
+  #'(begin-for-syntax
+      (set! a 44)))
+
+(n2)
+(m) 
+
+
+
+
+
+
+  
